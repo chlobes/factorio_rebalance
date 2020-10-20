d91b78d4003df7f6f10480e0daa40cfc76d16005
terepy <terepy@protonmail.com>
2020-10-20 15:33:09 +1100

    detect invalid entities

--- control.lua
+++ control.lua
@@ -13,7 +13,7 @@ local module_stats = {
 	["productivity-module-2"] = { eff =-3, spd =-1, prd = 8, pol = 2, },
 	["productivity-module-3"] = { eff =-4, spd =-3, prd = 14, pol = 4, },
 }
-script.on_init(function() if not global.beacons then global.beacons = {} end end)
+script.on_init(function() if not global.beacons then global.beacons = {} end end) --global.beacons is a hashmap of entity_id -> (entity, beacon) that tracks all the entities
 script.on_nth_tick(300, function()
 	local i = 0
 	local j = 0
@@ -33,38 +33,43 @@ script.on_nth_tick(300, function()
 	local lab_spd_bonus = j * 4
 	local lab_eff_bonus = 0
 	if j > 2 then lab_eff_bonus = 2 * (j - 2) end
-	for _, v in pairs(global.beacons) do
-		local output = v[2].get_module_inventory()
-		output.clear()
-		local eff = 0 local spd = 0 local prd = 0 local pol = 0
-		for item, stack in pairs(v[1].get_module_inventory().get_contents()) do
-			local x = module_stats[item]
-			eff = eff + (x.eff or 0) * stack
-			spd = spd + (x.spd or 0) * stack
-			prd = prd + (x.prd or 0) * stack
-			pol = pol + (x.pol or 0) * stack
-		end
-		if v[1].type == "lab" then
-			spd = spd + lab_spd_bonus
-			eff = eff + lab_eff_bonus
-		end
-		local p = 112
-		if spd - eff ~= 0 then output.insert{name=tostring(spd-eff+p-sign(spd-eff))} end
-		if spd ~= 0 then output.insert{name=tostring(spd+p*3-sign(spd))} end
-		if pol ~= 0 then output.insert{name=tostring(pol+p*7-sign(pol))} end
-		if v[1].bonus_mining_progress then
-			prd = 1.01^(prd+mining_prd_bonus) - 1 - 0.01*mining_prd_bonus
-			local v = 20.48
-			local n = 12
-			for i=1,n,1 do
-				if prd > v then
-					prd = prd - v
-					output.insert{name="mining-prd-"..i}
+	for k, v in pairs(global.beacons) do
+		if v[1].valid and v[2].valid then --TODO: what causes entities to become invalid without being destroyed?
+			local output = v[2].get_module_inventory()
+			output.clear()
+			local eff = 0 local spd = 0 local prd = 0 local pol = 0
+			for item, stack in pairs(v[1].get_module_inventory().get_contents()) do
+				local x = module_stats[item]
+				eff = eff + (x.eff or 0) * stack
+				spd = spd + (x.spd or 0) * stack
+				prd = prd + (x.prd or 0) * stack
+				pol = pol + (x.pol or 0) * stack
+			end
+			if v[1].type == "lab" then
+				spd = spd + lab_spd_bonus
+				eff = eff + lab_eff_bonus
+			end
+			local p = 112
+			if spd - eff ~= 0 then output.insert{name=tostring(spd-eff+p-sign(spd-eff))} end
+			if spd ~= 0 then output.insert{name=tostring(spd+p*3-sign(spd))} end
+			if pol ~= 0 then output.insert{name=tostring(pol+p*7-sign(pol))} end
+			if v[1].bonus_mining_progress then --special calculation for mining drills/oil pumps to account for mining tech
+				prd = 1.01^(prd+mining_prd_bonus) - 1 - 0.01*mining_prd_bonus
+				local v = 20.48
+				local n = 12
+				for i=1,n,1 do
+					if prd > v then
+						prd = prd - v
+						output.insert{name="mining-prd-"..i}
+					end
+					v = v / 2
 				end
-				v = v / 2
+			else
+				if prd ~= 0 then output.insert{name=tostring(prd+p*5-sign(prd))} end
 			end
 		else
-			if prd ~= 0 then output.insert{name=tostring(prd+p*5-sign(prd))} end
+			if v[2].valid then v[2].destroy() end
+			global.beacons[k] = nil
 		end
 	end
 end)
@@ -79,11 +84,8 @@ end
 function dead_crafter(event)
 	local entity = event.entity.unit_number
 	if global.beacons[entity] then
-		if global.beacons[entity][2].destroy() then
-			global.beacons[entity] = nil
-		else 
-			game.print("rebalance mod error: failed to destroy beacon")
-		end
+		global.beacons[entity][2].destroy()
+		global.beacons[entity] = nil
 	end
 end
 local filter = {
@@ -93,6 +95,8 @@ local filter = {
 }
 script.on_event(defines.events.on_built_entity, new_crafter, filter)
 script.on_event(defines.events.on_robot_built_entity, new_crafter, filter)
+script.on_event(defines.events.script_raised_built, new_crafter, filter)
 script.on_event(defines.events.on_entity_died, dead_crafter, filter)
 script.on_event(defines.events.on_player_mined_entity, dead_crafter, filter)
 script.on_event(defines.events.on_robot_mined_entity, dead_crafter, filter)
+script.on_event(defines.events.script_raised_destroy, dead_crafter, filter)
